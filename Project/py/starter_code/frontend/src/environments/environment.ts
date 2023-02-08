837808cdb4cdbc825a9f714656fa85cc4c19b928
ABISOLA TAYO <tayoabisola44@gmail.com>
2023-02-08 09:18:04 +0100

    final

--- Project/py/starter_code/frontend/src/environments/environment.ts
+++ Project/py/starter_code/frontend/src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
     url: 'dev-o1wdglbml3ts8wb2.us', // the auth0 domain prefix
-    audience: 'coffee image', // the audience set for the auth0 app
+    audience: 'image', // the audience set for the auth0 app
     clientId: '3qNJ8IJgYvGucBH4x0GiZ1q5Jj8UeoVI', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100/drinks', // the base url of the running ionic application. 
   }
